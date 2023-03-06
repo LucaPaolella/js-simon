7f612c0863df467c4b3fe7e2ceeefb0ec21245f0
Your Name <you@example.com>
2023-03-06 16:05:45 +0100

    i numeri spariscono dopo 30 secondi

--- js/main.js
+++ js/main.js
@@ -1,12 +1,21 @@
 "use strict";
-
+//Step 1
 let randomNum = [];
 for (let i=1; i<=5; i++) {
     randomNum.push(Math.round(Math.random() * 1000)+1)
 }
 console.log(randomNum)
-
+//Step 2
 document.querySelector ('h1').innerHTML = randomNum;
+
+//Step 3
+
+const indovinaNum = document.getElementById("indovina-num");
+
+setTimeout(function () {
+    indovinaNum.classList.add("none");;
+}, 30000);
+
 
 
 /*function createBombs(nBombs, min, max) {
