aa78025d6aececd241d9887d2732ff63acfb9803
Your Name <you@example.com>
2023-03-06 15:47:39 +0100

    creato array di 5 numeri casuali

---
+++ js/main.js
@@ -0,0 +1,23 @@
+"use strict";
+
+let randomNum = [];
+for (let i=1; i<=5; i++) {
+    randomNum.push(Math.round(Math.random() * 1000)+1)
+}
+console.log(randomNum)
+
+
+/*function createBombs(nBombs, min, max) {
+    const bombList = []; //0
+
+    while (bombList.length < nBombs) {
+        const bombNumber = getRndInteger(min, max);
+
+        if (!bombList.includes(bombNumber)) {
+            bombList.push(bombNumber);
+        }
+    }
+
+    return bombList;
+
+}*/
